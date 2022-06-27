ebf151ce5be258c914a7a6318a85c8540a70ff78
shuntaro1Q88 <shunexxx88@gmail.com>
2022-06-28 01:38:03 +0900

    add guest login

--- dev-docker-compose/schedule-board/src/main/java/com/shunproduct/scheduleboard/util/Role.java
+++ dev-docker-compose/schedule-board/src/main/java/com/shunproduct/scheduleboard/util/Role.java
@@ -2,9 +2,9 @@ package com.shunproduct.scheduleboard.util;
 
 public enum Role {
 	
-	   ADMIN(0, "管理"),
-	   USER(1, "編集"),
-	   GUEST(2, "閲覧"),
+	   ADMIN(0, "admin"),
+	   USER(1, "user"),
+	   GUEST(2, "guest"),
 	   ;
 
 	   private final int value;
--- dev-local/schedule-board/src/main/java/com/shunproduct/scheduleboard/config/SecurityConfig.java
+++ dev-local/schedule-board/src/main/java/com/shunproduct/scheduleboard/config/SecurityConfig.java
@@ -1,30 +1,14 @@
 package com.shunproduct.scheduleboard.config;
 
-import javax.sql.DataSource;
-
-
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.*;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
 import com.shunproduct.scheduleboard.util.Role;
 
 import lombok.RequiredArgsConstructor;
@@ -34,8 +18,6 @@ import lombok.RequiredArgsConstructor;
 @EnableWebSecurity
 public class SecurityConfig {
 
-	private final UserDetailsService userDetailsService;
-
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
     	// セキュリティ設定を無視するパスを指定
@@ -44,20 +26,25 @@ public class SecurityConfig {
 
     @Bean
 	protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.formLogin(login -> login
+        http
+        .formLogin(login -> login
                 .loginProcessingUrl("/login")
                 .loginPage("/login")
                 .defaultSuccessUrl("/")
                 .failureUrl("/login?error")
                 .permitAll()
-        ).logout(logout -> logout
+        )
+        .logout(logout -> logout
                 .logoutSuccessUrl("/login?logout")
-        ).authorizeHttpRequests(authz -> authz
+        )
+        .authorizeHttpRequests(authz -> authz
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                 .mvcMatchers("/login", "/user-register", "/error").permitAll()
                 .mvcMatchers("/admin/**").hasRole(Role.ADMIN.getRoleName())
                 .anyRequest().authenticated()
-        ).headers().frameOptions().sameOrigin();
+        )
+        .headers().frameOptions().sameOrigin();
+        
         return http.build();
 	}
     
