c72c9320c161cffb862454c60439334098b13369
shuntaro1Q88 <shunexxx88@gmail.com>
2022-07-16 11:36:23 +0900

    予定登録フォーム内js修正 #5

---
+++ prod-ecs-rds/schedule-board/src/main/java/com/shunproduct/scheduleboard/util/ScheduleDisplayParamDefaultConst.java
@@ -0,0 +1,20 @@
+package com.shunproduct.scheduleboard.util;
+
+public class ScheduleDisplayParamDefaultConst {
+
+	public ScheduleDisplayParamDefaultConst() {
+	}
+	
+	// デフォルトの表示するグループ
+	public static final String GROUPID = "1";
+	
+	/* 
+	 * DEVICEによる表示日数
+	 */
+	
+	// SPのデフォルトの表示する期間
+	public static final String DISPLAYTERM_SP = "7";
+	// PCとTABLETのデフォルトの表示する期間
+	public static final String DISPLAYTERM = "28";
+	
+}
---
+++ prod-ecs-rds/schedule-board/src/test/java/com/shunproduct/scheduleboard/ScheduleBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shunproduct.scheduleboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
